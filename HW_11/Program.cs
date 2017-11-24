49b34e08e835350543b4b33fa6701e4021ef6f51
Slidejiveman <rdnotlaw91@outlook.com>
2017-11-24 13:03:25 -0600

    update

--- HW_11/Program.cs
+++ HW_11/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using Dapper;
+using System.Data.SqlClient;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HW_11
 {
@@ -10,6 +11,45 @@ namespace HW_11
     {
         static void Main(string[] args)
         {
+            //Connection String to the database as a whole.
+            const string CONNECTION_STRING = @"Data Source = .\SQLEXPRESS;" +
+                                             @"Initial Catalog = Homework_11;" +
+                                             @"Integrated Security = True;" +
+                                             @"Connect Timeout = 15;" +
+                                             @"Encrypt = False;" +
+                                             @"TrustServerCertificate = True;" +
+                                             @"ApplicationIntent = ReadWrite;" +
+                                             @"MultiSubnetFailover = False";
+
+            using(IDbConnection conn = new SqlConnection(CONNECTION_STRING))
+            {
+                conn.Open();
+                using (var tran = conn.BeginTransaction())
+                {
+                    try
+                    {
+                        // DB requests go here
+                        conn.Execute("DELETE FROM lgcustomer WHERE cust_code = 730", transaction: tran);
+                        conn.Execute("DELETE FROM lgbrand WHERE brand_id = 23", transaction: tran);
+
+                        tran.Commit();
+                    } catch (Exception Ex)
+                    {
+                        Console.WriteLine(Ex.Message);
+                        try
+                        {
+                            tran.Rollback(); // If an exception is raised by the FK_Constraint, then enter this block.
+                            Console.WriteLine("Transaction rolled back.");
+                        }
+                        catch (SqlException SqlEx)
+                        {
+                            Console.WriteLine(SqlEx.Message);
+                            Console.WriteLine("The transaction was already rolled back by the DBMS.");
+                        
+                        }
+                    }
+                }
+            }
         }
     }
 }
---
+++ HW_11/lgcustomer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_11
+{
+    class lgcustomer
+    {
+        public int Cust_Code { get; set; }
+        public string Cust_Fname { get; set; }
+        public string Cust_Lname { get; set; }
+        public string Cust_Street { get; set; }
+        public string Cust_City { get; set; }
+        public string Cust_State { get; set; }
+        public string Cust_Zip { get; set; }
+        public float Cust_Balance { get; set; }
+    }
+}
